47bc78a3c6359fd42f5016dce1e59b0e4ac6583a
tyo gunawan <tyo@code.id>
2019-09-24 17:35:49 +0700

    create detail activity

---
+++ app/src/main/java/id/code/klikmovies/function/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package id.code.klikmovies.function.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.code.klikmovies.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
