1795aa06e58d6378bb540bd8eed5c0aca127a324
tyo gunawan <tyo@code.id>
2019-09-26 17:42:49 +0700

    save movie

--- app/src/main/java/id/code/klikmovies/util/PrefManager.kt
+++ app/src/main/java/id/code/klikmovies/util/PrefManager.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import id.code.klikmovies.model.Genre
+import id.code.klikmovies.model.Movie
 
 /**
  * Created by Tyo CODEID on 12/1/2016.
@@ -34,11 +35,35 @@ class PrefManager(private val _context: Context) {
         editor.commit()
     }
 
+    fun saveMovieAsFavorite(movie: Movie) {
+        var moviesSaved: MutableList<Movie>
+        val data = pref.getString(FAVORITE, "")
+        val gson = GsonBuilder().create()
+        moviesSaved = gson.fromJson(data, MutableList()<Movie>::class.java).toMutableList()
+        moviesSaved.add(movie)
+        saveMovieFavoriteToJson(moviesSaved)
+    }
+
+    fun getFavoriteMovie () : List<Movie>?{
+        val data = pref.getString(FAVORITE, "")
+        val gson = GsonBuilder().create()
+        val movies : List<Movie> = gson.fromJson(data, Array<Movie>::class.java).toList()
+        return movies
+    }
+
+    fun saveMovieFavoriteToJson(movie: MutableList<Movie>?) {
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        val jsonMovie: String = gson.toJson(movie)
+        editor.putString(FAVORITE, jsonMovie)
+        editor.commit()
+    }
+
 
     companion object {
         private val PRIVATE_MODE = 0
         private val PREF_NAME = "Preferences"
         private val GENRE = "genre_list"
+        private val FAVORITE = "favorite"
     }
 
 }
